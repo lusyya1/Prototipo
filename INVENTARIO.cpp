944c106b616362b6d33e06be0559ca57d1abb1b2
Michelle Juárez López <michellejuarez.uaneg@ms.uas.edu.mx>
2025-12-03 14:32:01 -0700

    avance punteros

--- INVENTARIO.cpp
+++ INVENTARIO.cpp
@@ -33,5 +33,41 @@ Producto inventario[TOTAL] = {
     {109, "Yogurt", 8.50, 60},
     {110, "Chocolate", 14.00, 80}
 };
+
 /* --------------------------------------------------------------------------------
    FUNCIONES
+-----------------------------------------------------------------------------------*/ 
+void mostrarInventario() { // Esta función solo imprime la lista.
+    cout << "\n--- LISTA DE PRODUCTOS ---\n";
+    cout << "ID\tNombre\t\tPrecio\tCantidad\n"; 
+    cout << "------------------------------------------\n";
+    
+    // Recorro todo el arreglo uno por uno
+    for(int i = 0; i < TOTAL; i++) {
+        // Accedo a los datos usando el punto (.) porque aquí NO son punteros
+            cout << inventario[i].id << "\t"  // Uso el \t (tabulador) para que se vea mas bonito
+             << inventario[i].nombre << "\t\t$" 
+             << inventario[i].precio << "\t" 
+             << inventario[i].cantidad << "\n";
+    }
+    cout << "------------------------------------------\n";
+}
+
+// -- PUNTEROS --
+// La función recibe un PUNTERO (*prod), osea, no recibe una copia del producto, 
+// recibe la dirección de memoria original, esto hace que cualquier cambio que haga 
+// se refleje en el arreglo original.
+void modificarStock(Producto* prod) {
+    int nuevaCantidad;
+    
+    // Como 'prod' es un puntero, tengo que usar la flechita (->) significa "ve a la dirección y saca el dato".
+    cout << "Producto seleccionado: " << prod->nombre << "\n"; 
+    cout << "Stock actual: " << prod->cantidad << "\n";
+    cout << "Ingresa la nueva cantidad: ";
+    cin >> nuevaCantidad;
+
+    // Aquí actualizo el valor. Como es puntero, se guarda directo en el original.
+    prod->cantidad = nuevaCantidad; 
+    cout << "¡Stock actualizado!\n";
+}
+
