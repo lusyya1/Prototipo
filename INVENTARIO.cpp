5014677ea942cca44062b873ee4ebc94ee7fe57d
Michelle Juárez López <michellejuarez.uaneg@ms.uas.edu.mx>
2025-12-03 20:05:21 -0700

    Final

--- INVENTARIO.cpp
+++ INVENTARIO.cpp
@@ -71,3 +71,76 @@ void modificarStock(Producto* prod) {
     cout << "¡Stock actualizado!\n";
 }
 
+// -- MAIN --
+int main() {
+    int opcion = 0;
+    int idBusqueda;
+
+    // Puse todo en un do-while para que el menú no se cierre rapido.
+    // Se repite hasta que elija la opción 5.
+    do {
+        cout << "\n=== MI TIENDITA ===\n";
+        cout << "1. Ver inventario\n";
+        cout << "2. Buscar por ID y editar Stock (Uso punteros y excepciones)\n";
+        cout << "3. Ordenar por Precio (Burbuja)\n";
+        cout << "4. Ordenar por Cantidad (Burbuja)\n";
+        cout << "5. Salir\n";
+        cout << "Que quieres hacer: ";
+        cin >> opcion;
+
+        // Por si el usuario escribe letras en vez de números
+        if(cin.fail()) {
+            cin.clear(); // Limpio el error 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignoro lo que escribí
+            cout << "Escribe un numero, por favor.\n";
+            continue; // Vuelvo a empezar el ciclo
+        }
+
+        switch(opcion) {
+            case 1:
+                mostrarInventario();
+                break;
+
+            case 2:
+                // -- MANEJO DE EXCEPCIONES --
+                // Aquí uso el bloque try-catch para manejar errores.
+                // La idea es: "intenta hacer esto (el try) y si algo sale mal (el throw), atrápalo".
+                cout << "\nIngresa el ID a buscar: ";
+                cin >> idBusqueda;
+
+                try {
+                    // Intento buscar. Si el ID no existe, la función 'buscarPorID' 
+                    // lanza el error y saltamos directo al 'catch'.
+                    Producto* prodEncontrado = buscarPorID(idBusqueda);
+                    
+                    cout << "¡Encontrado! Es: " << prodEncontrado->nombre << "\n";
+                    
+                    // Si llegué aquí es que sí existe. paso el puntero a la función de modificar.
+                    modificarStock(prodEncontrado);
+
+                } catch (const char* mensajeError) {
+                    // Aquí caigo si el ID no existía. Imprimo el mensaje que puse en el throw.
+                    cout << "\n*** OCURRIO UN ERROR ***\n";
+                    cout << mensajeError << "\n";
+                }
+                break;
+
+            case 3:
+                ordenarPorPrecio();
+                break;
+
+            case 4:
+                ordenarPorCantidad();
+                break;
+
+            case 5:
+                cout << "Adios.\n";
+                break;
+
+            default:
+                cout << "Esa opcion no existe.\n";
+        }
+
+    } while(opcion != 5);
+
+    return 0;
