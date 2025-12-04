d5a76864c316d217e7659c5a7f6c2b488aeb0589
Michelle Juárez López <michellejuarez.uaneg@ms.uas.edu.mx>
2025-12-04 00:16:35 -0700

    INCIO BASICO

---
+++ CALCULADORA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath> // Necesito esta libreria para usar sqrt (la raiz cuadrada) y pow
+
+using namespace std;
+
+/* --------------------------------------------------------------------------------
+   PROTOTIPOS DE FUNCIONES
+-----------------------------------------------------------------------------------*/
+
+double sumar(double a, double b);                // Suma normal de 2 numeros
+double sumar(double a, double b, double c);      // Suma de 3 numeros  
+
+double restar(double a, double b);
+double multiplicar(double a, double b);
+double dividir(double a, double b);
+
